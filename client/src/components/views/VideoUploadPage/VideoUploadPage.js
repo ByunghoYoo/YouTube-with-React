84a158852bc3003785aa458942af80c37a3aee48
ByunghoYoo <66407407+ByunghoYoo@users.noreply.github.com>
2021-02-16 23:03:40 +0900

    add upload video task

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd'
 import Dropzone from 'react-dropzone'
+import Axios from 'axios'
 
 const { TextArea } = Input
 const { Title } = Typography
 
+const PrivateOptions = [
+    { value: 0, label: "Private" },
+    { value: 1, label: "Public" }
+]
+
+const CategoryOptions = [
+    { value: 0, label: "Film & Animation" },
+    { value: 1, label: "Auto & Vehicles" },
+    { value: 2, label: "Music" },
+    { value: 3, label: "Pets & Animals" }
+]
+
 function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Film & Animation")
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value)
+    }
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value)
+    }
+
+    const onDrop = (files) => {
+        let formData = new FormData
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+        
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if(response.data.success) {
+                    console.log('test: ',response.data)
+                } else {
+                    alert('비디어 업로드를 실패했습니다.')
+                }
+            })
+        
+    }
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -16,9 +69,9 @@ function VideoUploadPage() {
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     {/* Drop zone */}
                     <Dropzone
-                    onDrop
-                    multiple
-                    maxSize
+                    onDrop={onDrop}
+                    multiple={false}
+                    maxSize={100000000}
                     >
                         {({ getRootProps, getInputProps }) => (
                             <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex',
@@ -40,27 +93,31 @@ function VideoUploadPage() {
             <br/>
             <label>Title</label>
             <Input
-                onChange
-                value
+                onChange={onTitleChange}
+                value={VideoTitle}
             />
             <br/>
             <br/>
             <label>Description</label>
             <TextArea
-                onChange
-                value
+                onChange={onDescriptionChange}
+                value={Description}
             />
             </Form>
             <br/>
             <br/>
 
-            <select onChange>
-                <opiton key value></opiton>
+            <select onChange={onPrivateChange}>
+                {PrivateOptions.map((item, index) => (
+                    <option key={index} value={item.value}>{item.label}</option>
+                ))}
             </select>
             <br/>
             <br/>
-            <select onChange>
-                <opiton key value></opiton>
+            <select onChange={onCategoryChange}>
+                {CategoryOptions.map((item, index) => (
+                    <option key={index} value={item.value}>{item.label}</option>
+                ))}
             </select>   
             <br/>
             <br/>
