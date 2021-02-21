2f87252d249dc3706614ca17d60f231c6efa2cbf
ByunghoYoo <66407407+ByunghoYoo@users.noreply.github.com>
2021-02-21 19:24:39 +0900

    add LandingPage &  VideoDetailPage

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd'
 import Dropzone from 'react-dropzone'
 import Axios from 'axios'
+import { useSelector } from 'react-redux'
 
 const { TextArea } = Input
 const { Title } = Typography
@@ -18,12 +19,16 @@ const CategoryOptions = [
     { value: 3, label: "Pets & Animals" }
 ]
 
-function VideoUploadPage() {
+function VideoUploadPage(props) {
 
+    const user = useSelector(state => state.user)
     const [VideoTitle, setVideoTitle] = useState("")
     const [Description, setDescription] = useState("")
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
 
     const onTitleChange = (e) => {
         setVideoTitle(e.currentTarget.value)
@@ -52,11 +57,60 @@ function VideoUploadPage() {
             .then(response => {
                 if(response.data.success) {
                     console.log('test: ',response.data)
+
+                    let variable = {
+                        url: response.data.url,
+                        fileName: response.data.fileName
+                    }
+
+                    setFilePath(response.data.url)
+
+                    Axios.post('/api/video/thumbnail', variable)
+                        .then(response => {
+                            if(response.data.success) {
+                                console.log('test2: ', response.data)
+                                if(response.data.success) {
+                                    setDuration(response.data.fileDuration)
+                                    setThumbnailPath(response.data.url)
+                                }
+
+                            } else {
+                                alert('썸네일 생성에 실패했습니다.')
+                            }
+                        })
+
                 } else {
                     alert('비디어 업로드를 실패했습니다.')
                 }
             })
-        
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const variables = {
+            writer: user.userData._id,
+            title: VideoTitle,
+            description: Description,
+            privacy: Private,
+            filePath: FilePath,
+            category: Category,
+            duration: Duration,
+            thumbnail: ThumbnailPath,
+        }
+
+        Axios.post('/api/video/uploadVideo', variables)
+            .then(response => {
+                if(response.data.success) {
+                    message.success('성공적으로 업로드를 했습니다.')
+                    
+                    setTimeout(() => {
+                        props.history.push('/')
+                    }, 3000);
+                } else {
+                    alert('비디오 업로드에 실패했습니다.')
+                }
+            })
     }
 
     return (
@@ -65,7 +119,7 @@ function VideoUploadPage() {
                 <Title level={2}>Upload Video</Title>
             </div>
 
-            <Form onSubmit>
+            <Form onSubmit={onSubmit}>
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     {/* Drop zone */}
                     <Dropzone
@@ -82,11 +136,12 @@ function VideoUploadPage() {
                         )}
 
                     </Dropzone>
-
                     {/* Thumbnail */}
+                    {ThumbnailPath &&
                     <div>
-                        <img src alt />
+                        <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail" />
                     </div>
+                    }
                 </div>
             
             <br/>
@@ -121,7 +176,7 @@ function VideoUploadPage() {
             </select>   
             <br/>
             <br/>
-            <Button type="primary" size="large" onClick>
+            <Button type="primary" size="large" onClick={onSubmit}>
                 Submit
             </Button>
         </div>
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-//const { Video } = require("../models/Video");
+const { Video } = require("../models/Video");
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer")
+const ffmpeg = require("fluent-ffmpeg")
 
 //=================================
 //             Video
@@ -34,6 +35,79 @@ router.post('/uploadfiles', (req, res) => {
             return res.json({ success: false, err })
         }
         return res.json({ success: true, url: res.req.file.path, fileName: res.req.file.fileName })
+    })
+})
+
+router.post('/uploadVideo', (req, res) => {
+    // 비디오 정보를 DB에 저장한다.
+    const video = new Video(req.body)
+
+    video.save((err, doc) => {
+        if(err) return res.json({ success: false, err })
+        res.status(200).json({ success: true })
+    })
+
+})
+
+router.get('/getVideos', (req, res) => {
+    // 비디오 정보를 DB에 가져와서 클라이언트로 보낸다.
+
+    // Video Collection을 생성할 때 writer 필드에 User를 참조하도록 하였는데
+    // populate를 해주어야 writer에 관한 모든 User정보를 가지고 올 수 있음
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err)
+            res.status(200).json({ success: true, videos })
+        })
+})
+
+router.post('/getVideoDetail', (req, res) => {
+
+    Video.findOne({ "_id": req.body.videoId })
+        .populate('writer')
+        .exec((err, videoDetail) => {
+            if(err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, videoDetail })
+        })
+})
+
+router.post('/thumbnail', (req, res) => {
+    
+    // 썸네일 생성하고 비디오 러닝타임 가져오기
+    let filePath = ""
+    let fileDuration = ""
+
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function(err, metadata) {
+        console.dir(metadata)
+        console.log(metadata.format.duration)
+        fileDuration = metadata.format.duration
+    })
+
+    // 썸네일 생성
+    ffmpeg(req.body.url)
+    .on('filenames', function(filenames) {
+        console.log('Will generate ' + filenames.join(', '))
+        console.log(filenames)
+
+        filePath = "uploads/thumbnails/" + filenames[0]
+    })
+    .on('end', function() {
+        console.log('Screenshots taken');
+        return res.json({ success: true, url: filePath, fileDuration: fileDuration })
+    })
+    .on('error', function(err) {
+        console.log(err)
+        return res.json({ success: false, err })
+    })
+    .screenshots({
+        // Will take screenshots at 20%, 40%, 60% and 80% of the video
+        count: 3,
+        folder: 'uploads/thumbnails',
+        size: '320x240',
+        // '%b : input basename (filename w/o extention)
+        filename: 'thumbnail-%b.png'
     })
 
 })
